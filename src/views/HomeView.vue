55443e54ed82e247c9170a808057fecb2b56c1bf
opmvpc <thibsix@outlook.be>
2022-08-12 18:18:04 +0200

    project bootstrap

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <input type="file" id="file-input-signature" accept="image/png, image/jpeg">
+    <input type="file" id="file-input-img" accept="image/png, image/jpeg">
+  </main>
+</template>
