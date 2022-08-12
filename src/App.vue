1b2b8e2f9b4563428c6f7a9f4f524ca3092a179e
opmvpc <thibsix@outlook.be>
2022-08-13 01:32:33 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,18 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </header>
+  <main class="px-3">
+    <header>
+      <nav class="flex items-center justify-between">
+        <h1 class="text-2xl font-mono">Signature</h1>
+        <div class="flex space-x-6">
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </div>
+      </nav>
+    </header>
 
-  <RouterView />
+    <RouterView class="mt-10" />
+  </main>
+
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const signature = undefined;
+const image = undefined
+ref(signature);
+ref(image);
+
+
+const signatureUploaded = () => {
+  console.log(signature);
+
+}
+
 </script>
 
 <template>
   <main>
-    <input type="file" id="file-input-signature" accept="image/png, image/jpeg">
-    <input type="file" id="file-input-img" accept="image/png, image/jpeg">
+    <form class="flex flex-col">
+      <input @blur="signatureUploaded()" type="file" id="file-input-signature" accept="image/png, image/jpeg">
+      <input type="file" id="file-input-img" accept="image/png, image/jpeg">
+    </form>
   </main>
 </template>
